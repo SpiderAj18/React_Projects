4cd5937d96312ae6f2705104d37d67f59ab16618
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-10-07 13:05:30 +0000

    card config

--- 03themeChanger/src/component/Card.jsx
+++ 03themeChanger/src/component/Card.jsx
@@ -3,12 +3,12 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                <video controls className="p-8 rounded-t-lg" src="https://player.vimeo.com/progressive_redirect/playback/868589497/rendition/360p/file.mp4?loc=external&oauth2_token_id=57447761&signature=d04d456d9f2c67cc9da989159b1942ecf449f6faa674160baacf7589b6e0a8dc" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                        Calmness of the night, truns into rush at morning
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
