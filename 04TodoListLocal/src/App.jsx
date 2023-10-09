dac363cc32f41019ae1d9c793a657cce637790ab
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-10-09 07:20:58 +0000

    functionality added

--- 04TodoListLocal/src/App.jsx
+++ 04TodoListLocal/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import {TodoProvider,} from "./context" 
+import TodoItem from './component/TodoItems'
+import TodoForm from './component/TodoForm'
 
 function App() {
   const[todos, setTodos]= useState([])
@@ -19,6 +21,20 @@ function App() {
     setTodos((prev)=>prev.map((prevTodo)=>prevTodo===id ? {...prev,completed:!prevTodo.completed} : prevTodo))
   }
 
+
+  //creating localStorage for list
+  useEffect(()=>{
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if(todos&& todos.length>0){
+      setTodos(todos)
+    }
+  },[])
+  useEffect(()=>{
+    localStorage.setItem("todos",JSON.stringify(todos))
+  },[todos])
+
+
   
   return (
     <TodoProvider value={{todos,addTodo,editTodo,deleteTodo,toggleList}}>
@@ -27,9 +43,16 @@ function App() {
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
+                        <TodoForm/>
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo)=>(
+                          <div key={todo.id}
+                          className='w-full'>
+                            <TodoItem todo={todo}></TodoItem>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
--- 04TodoListLocal/src/context/TodoContext.js
+++ 04TodoListLocal/src/context/TodoContext.js
@@ -1,23 +1,22 @@
-import React, { useContext,createContext } from "react";
+import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    Todos:[
-        { 
-          id:1,
-          todo:"this is msg",
-          completed:false,
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
         }
     ],
     addTodo: (todo) => {},
-    editTodo: (id,todo) => {},
+    updateTodo: (id, todo) => {},
     deleteTodo: (id) => {},
-    todoComplete:(id) => {},
-    toggleList:(id) =>{},
+    toggleComplete: (id) => {}
+})
 
-});
 
-export const useTodo = ()=>{
-    return useContext(TodoContext);
+export const useTodo = () => {
+    return useContext(TodoContext)
 }
 
 export const TodoProvider = TodoContext.Provider
