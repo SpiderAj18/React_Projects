7068759fe5318bd10480462cc70c18681161aed7
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-10-12 08:10:23 +0000

    configed

--- 05reduxToolkit/src/App.jsx
+++ 05reduxToolkit/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodos from "./components/AddTodo"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
     React with redux 
+    <AddTodos></AddTodos>
+    
     </>
   )
 }
