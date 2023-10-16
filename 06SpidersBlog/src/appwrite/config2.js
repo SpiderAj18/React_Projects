3add2a09bea0c62343c8e9890b1ebbb356c77f45
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-10-16 10:43:27 +0000

    new change

--- 06SpidersBlog/src/appwrite/config2.js
+++ 06SpidersBlog/src/appwrite/config2.js
@@ -16,8 +16,51 @@ export class DatabaseService{
     }
     async createPost({title, slug, content, featuredImage, status, userId}){
         try{
-            return await this.databases.createDocument
+            return await this.databases.createDocument(
+                config.DatabaseId,
+                config.collectionId,
+                slug,{
+                    title,content,
+                    featuredImage,
+                    status,
+                    userId,
+                }
+            )
+        }catch(error){
+            console.log("Appwrite service :: createPost :: error",error);
         }
+    }
+
+    //update post 
+    async updatePost(slug,{title,  content,featuredImage ,status}){
+        try {
+            return await this.databases.updateDocument(
+                config.DatabaseId,
+                config.collectionId,
+                slug,{
+                    title,
+                    content,
+                    featuredImage,status,
+                }
+            )
+        } catch (error) {
+            console.log("aapwrite :: updatepost :: error",error);
+        }
+    }
+
+    //delete document 
+    async deletePost({slug}){
+        try {
+            return await this.databases.deleteDocument(
+                config.DatabaseId,
+                config.collectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("appwrite service :: delete doc :: error",error);
+            return false;
+        }
+
     }
 }
 
