a63d3586d4e1cf2cd3d3d41b879b830ae3ca026e
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-10-27 18:08:53 +0000

    productiongradecongifs

--- 06SpidersBlog/src/components/Header/logoutBtn.jsx
+++ 06SpidersBlog/src/components/Header/logoutBtn.jsx
@@ -6,10 +6,12 @@ import {logout} from "../../store/authSlice"
 function logoutBtn() {
   const dispatch = useDispatch()
   const logoutHandler = () => {
-    authService.logout()
+    authService.logout().then(()=>{
+      dispatch(logout())
+    })
   }
   return (
-    <div>logoutBtn</div>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
   )
 }
 
