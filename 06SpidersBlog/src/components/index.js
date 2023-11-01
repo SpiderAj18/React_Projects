4339f77a6e0e90a97289c3fd984894b7cf7888fb
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-11-01 19:54:16 +0000

    configed

--- 06SpidersBlog/src/components/index.js
+++ 06SpidersBlog/src/components/index.js
@@ -2,10 +2,14 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import logoutBtn from './Header/logoutBtn';
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    RTE,
+    logoutBtn
 }
