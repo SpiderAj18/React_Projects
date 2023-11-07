c4bc30ce5c59e1c5de58791b12f3494ce3108af9
SpiderAj18 <130140695+SpiderAj18@users.noreply.github.com>
2023-11-07 06:08:46 +0000

    pages created

--- 06SpidersBlog/src/components/index.js
+++ 06SpidersBlog/src/components/index.js
@@ -4,6 +4,9 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import logoutBtn from './Header/logoutBtn';
 import RTE from "./RTE";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 export {
     Header,
@@ -11,5 +14,8 @@ export {
     Container,
     Logo,
     RTE,
-    logoutBtn
+    logoutBtn,
+    SignUp,
+    Home,
+    Login,
 }
---
+++ 06SpidersBlog/src/components/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Login as LoginasComponent } from '..';
+function Login(){
+    return(
+        <div className='py-10'>
+            <LoginasComponent/>
+        </div>
+    )
+}
+
+export default Login();
